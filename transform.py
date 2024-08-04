700cbdeb0414384aa2ddadb60b0c1841de1e53ed
marci314 <marci3141@gmail.com>
2024-08-04 12:17:21 +0200

    first version of transform

---
+++ transform.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# for further analysis, we are going to split table into multiple tables, where each table contains data only from one cryptocurrency
+def split_by_symbol(df):
+    symbol_dict = {}
+    for symbol in df['Symbol'].unique():
+        symbol_df = df[df['Symbol'] == symbol].copy()
+        symbol_dict[symbol] = symbol_df
+    
+    return symbol_dict
+
+import os
+
+def save_table(table, name):
+    os.makedirs("data", exist_ok=True)
+    file_name = f"{name}.csv"
+    table.to_csv(os.path.join("data", file_name), index=False)
+
+def save_tables_dict(table_dict):
+    os.makedirs("data", exist_ok=True)
+    for name, table in table_dict.items():
+        file_name = f"{name}.csv"
+        table.to_csv(os.path.join("data", file_name), index=False)
